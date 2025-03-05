781be815bedef3f69f46f08b7e97196a9fbd96ae
Alegarfer <garcia_ferreira@hotmail.com>
2025-03-05 16:59:14 -0400

    Add animação de Java Script

--- scrip.js
+++ scrip.js
@@ -1,58 +1,61 @@
 $(document).ready(function() {
-    $('mobile_btn').on('click', function() {
-        $('mobile_menu').toggleClass('active');
-        $('mobile_btn').find('i').toggleClass('fa-x');
-    });
-    const section = $('section');
-    const navItens = $('.nav-itens');
+  $('#mobile_btn').on('click', function () {
+      $('#mobile_menu').toggleClass('active');
+      $('#mobile_btn').find('i').toggleClass('fa-x');
+  });
 
-    $(window).on('scroll', function() {
-        const header = $('header');
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+  const sections = $('section');
+  const navItems = $('.nav-item');
 
-        let activeSectionIndex = 0;
-        if (scrollPosition <= 0) {
-            header.css('box-shadow','none');
-        } else {
-            header.css('box-shadow','5px 1px 5px rgba(0,0,0,0.4)');
-        }
+  $(window).on('scroll', function () { 
+      const header = $('header');
+      const scrollPosition = $(window).scrollTop() - header.outerHeight();
+      let activeSectionIndex = 0;
 
-        section.each(function(index) {
-            const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionButton = sectionTop + section.outerHeight();
-            if (scrollPosition >= sectionTop && scrollPosition < sectionButton) {
-                activeSectionIndex = index;
-                return false;
-            }
-        });
-        navItens.remove.Class('active');
-        $(navItens[activeSectionIndex]).addClass('active');
-    });
+      if (scrollPosition <= 0) {
+          header.css('box-shadow', 'none');
+      } else {
+          header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+      }
 
-    /* Animação */
+      sections.each(function(i) {
+          const section = $(this);
+          const sectionTop = section.offset().top - 96; 
+          const sectionBottom = sectionTop+ section.outerHeight();
 
-    scrollReavel().scrollReavel('#cta',{
-        origin:'left',
-        duratin:2000,
-        distance:'20%'
-    });
+          if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+              activeSectionIndex = i;
+              return false;
+          }
+      })
 
-    scrollReavel().scrollReavel('.burguer',{
-            origin:'left',
-            duratin:2000,
-            distance:'20%'
-    });
+      navItems.removeClass('active');
+      $(navItems[activeSectionIndex]).addClass('active');
+  });
 
-    scrollReavel().scrollReavel('#testimonials_burguer',{
-        origin:'left',
-        duratin:2000,
-        distance:'20%'
-    });
+  
+  /** Animação */
+  ScrollReveal().reveal('#cta', {
+      origin: 'left',
+      duration: 2000,
+      distance: '20%'
+  });
 
-    scrollReavel().scrollReavel('#feededback',{
-        origin:'right',
-        duratin:1000,
-        distance:'20%'
-});
+  ScrollReveal().reveal('.burguer', {
+      origin: 'left',
+      duration: 2000,
+      distance: '20%'
+  });
+
+  ScrollReveal().reveal('#testimonial_burguer', {
+      origin: 'left',
+      duration: 1000,
+      distance: '20%'
+  })
+
+  ScrollReveal().reveal('.feedback', {
+      origin: 'right',
+      duration: 1000,
+      distance: '20%'
+  })
 });
